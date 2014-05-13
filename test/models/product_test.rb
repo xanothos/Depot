699d8bcb11e7d1d97f211b688ba18c0f0f1ca6fe
Matthew Schouppe <mschoup@gmail.com>
2014-05-13 08:09:49 -0400

    Iteration B2

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "price must be positive" do
+    product = Product.new title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg'
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image url" do
+    ok = %w{bob.gif tom.png bobandtom.jpg http://this.and.that.com/a/b/c/picture.png}
+    bad = %w{random.gyf junk.jpig http://some.url.com/a/x/s/gif.pang}
+
+    ok.each do |name|
+      product = new_product name
+      assert product.valid?, "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      product = new_product name
+      assert product.invalid?, "#{name} should be invalid"
+    end
+
+
+  end
+
+  def new_product(image_url)
+    Product.new title: 'My test book', description: 'yyy', price: 1, image_url: image_url
+  end
 end

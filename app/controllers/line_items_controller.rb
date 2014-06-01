45f9bf3c7bcfd618e016daca41b71506366c8a1a
Matthew Schouppe <mschoup@gmail.com>
2014-05-31 21:43:03 -0400

    Iteration D3

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,7 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_cart, only: [:create]
 
   # GET /line_items
   # GET /line_items.json
@@ -24,11 +26,13 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        # why did we add .cart here instead of using @cart???
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }

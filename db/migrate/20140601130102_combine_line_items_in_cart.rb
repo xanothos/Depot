3acf1ba67a6881b2d4641f4bbc2c96afa81a7b6c
Matthew Schouppe <mschoup@gmail.com>
2014-06-03 22:46:27 -0400

    Ch10 - Iteration E1

--- db/migrate/20140601130102_combine_line_items_in_cart.rb
+++ db/migrate/20140601130102_combine_line_items_in_cart.rb
@@ -11,4 +11,13 @@ class CombineLineItemsInCart < ActiveRecord::Migration
       end
     end
   end
+
+  def down
+    LineItem.where("quantity > 1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create product_id: line_item.product_id, cart_id: line_item.cart_id, quantity: 1
+      end
+      line_item.destroy
+    end
+  end
 end

7ef34c05171a8be6c42b610dd8370394d371898b
Matthew Schouppe <mschoup@gmail.com>
2014-06-05 08:22:49 -0400

    Ch10 - Iteration E2 (continued)

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -55,9 +55,14 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    notice = nil
+    if @cart.id == session[:cart_id]
+      @cart.destroy
+      session[:cart_id] = nil
+      notice = 'Your cart is now empty.'
+    end
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url, notice: notice }
       format.json { head :no_content }
     end
   end

984a3265281acd6dd80355e4ed221bc3b637ba06
Matthew Schouppe <mschoup@gmail.com>
2014-06-04 22:15:09 -0400

    Ch10 - Iteration E2

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :redirect_to_home
 
   # GET /carts
   # GET /carts.json
@@ -70,5 +71,10 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
+    end
+
+    def redirect_to_home
+      logger.error "Attempt to access an invalid cart '#{params[:id]}'"
+      redirect_to store_url, notice: "That was an invalid cart!"
     end
 end

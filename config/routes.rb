1885eb51c8408f4d34db777056e6aee0cf6b1f90
Matthew Schouppe <mschoup@gmail.com>
2014-07-03 07:40:42 -0400

    Ch12 - Iteration G2

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   get 'store/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   root 'store#index', as: 'store'
 
